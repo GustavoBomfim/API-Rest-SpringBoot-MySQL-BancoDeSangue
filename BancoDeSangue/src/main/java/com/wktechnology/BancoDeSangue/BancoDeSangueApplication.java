e662938195f5c63f69a5c6caeff32643350b5a5f
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-02-13 12:52:03 -0300

    Depêndencias do projeto instaladas

---
+++ BancoDeSangue/src/main/java/com/wktechnology/BancoDeSangue/BancoDeSangueApplication.java
@@ -0,0 +1,13 @@
+package com.wktechnology.BancoDeSangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeSangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDeSangueApplication.class, args);
+	}
+
+}
---
+++ BancoDeSangue/src/test/java/com/wktechnology/BancoDeSangue/BancoDeSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wktechnology.BancoDeSangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeSangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
