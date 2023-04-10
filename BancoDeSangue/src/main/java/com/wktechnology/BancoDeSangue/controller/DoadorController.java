c8d6d001a85375b6d5155be1aad61c56a6e30548
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-04-10 19:02:20 -0300

    Criando o endpoint para listagem de todos os doadores do banco

--- BancoDeSangue/src/main/java/com/wktechnology/BancoDeSangue/controller/DoadorController.java
+++ BancoDeSangue/src/main/java/com/wktechnology/BancoDeSangue/controller/DoadorController.java
@@ -3,13 +3,13 @@ package com.wktechnology.BancoDeSangue.controller;
 import com.wktechnology.BancoDeSangue.doador.DadosCadastroDoador;
 import com.wktechnology.BancoDeSangue.doador.Doador;
 import com.wktechnology.BancoDeSangue.doador.DoadorRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doadores")
@@ -23,4 +23,11 @@ public class DoadorController {
         repository.save(new Doador(dados));
         System.out.println("Registros \n \n" +  dados);
     }
+
+    @GetMapping
+    public List<Doador> doadores(){
+        return repository.findAll();
+    }
+
+
 }
